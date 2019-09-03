8d26e8b3bf0ac73f74060f9e7215345a50888acf
Soham Patel <patesoh@gmail.com>
2019-09-03 10:17:04 -0400

    Minor updates and fixes
    Minor updates and fixes

--- HW14-JavaScript/static/js/app.js
+++ HW14-JavaScript/static/js/app.js
@@ -3,7 +3,7 @@ var tbody = d3.select("tbody");
 
 console.log(data);
 
-// Use D3 Refactor to populate the table with all data from data.js
+// Use D3 arrow function to populate the table with all data from data.js
 data.forEach((UFOReport) => {
     var row = tbody.append("tr");
     Object.entries(UFOReport).forEach(([key, value]) => {
@@ -13,24 +13,27 @@ data.forEach((UFOReport) => {
   });
 
 //   Use jquery datatable feature to build in a master search feature
-// and build a column
-$(document).ready(function() {
-    // Setup - add a text input to each footer cell
-    $('#ufo-table thead th').each( function () {
-        var title = $('#ufo-table tfoot th').eq( $(this).index() ).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-    } );
+// and build a column-specific search box for all columns in the table, that also has a sort feature, at the top of the table
+$('#ufo-table thead th').each( function () {
+    var title = $('#ufo-table tfoot th').eq( $(this).index() ).text();
+    $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+} );
 
-    // DataTable
-    var table = $('#ufo-table').DataTable();
+// DataTable
+var table = $('#ufo-table').DataTable();
 
-    // Apply the search
-    table.columns().eq( 0 ).each( function ( colIdx ) {
-        $( 'input', table.column( colIdx ).header() ).on( 'keyup change clear', function () {
-            table
-                .column( colIdx )
-                .search( this.value )
-                .draw();
-        } );
+// Apply the search
+table.columns().eq( 0 ).each( function ( colIdx ) {
+    $( 'input', table.column( colIdx ).header() ).on( 'keyup change', function () {
+        table
+            .column( colIdx )
+            .search( this.value )
+            .draw();
     } );
 } );
+
+// Keep the column headers on top
+$('#ufo-table tfoot tr').appendTo('#ufo-table thead');
+
+
+
